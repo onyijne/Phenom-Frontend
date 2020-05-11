e3e378a32f8fec9676d3979af8d7bb8458e98937
Samuel Onyijne <samuelord1@gmail.com>
2020-05-11 13:45:39 +0100

    feat: updated the template to react
    BREAKING CHANGE: removed parcel as it wasn't working with ionic

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,30 @@ module.exports = {
     node: true,
     jest: true
   },
-  extends: ['airbnb-base'],
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
     ecmaVersion: 2018,
     sourceType: 'module'
   },
+  plugins: [
+    "react"
+  ],
   rules: {
     'comma-dangle': ['error', 'never'],
-    'no-param-reassign': ['error', { props: false }]
+    'no-param-reassign': ['error', { props: false }],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "class-methods-use-this" : 0
   }
 };
--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
-const app = async () => '#BuildforSDG';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
 
-export default app;
+import Home from './pages/home.jsx';
+
+function App() {
+  return (
+    <IonApp>
+    <IonReactRouter>
+      <IonRouterOutlet>
+        <Route path="/" component={Home} exact={true} />
+      </IonRouterOutlet>
+    </IonReactRouter>
+  </IonApp>
+  );
+}
+
+export default App;
